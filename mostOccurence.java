c76d576c5587ab0447c12a224413b306c40c731d
ssneharathore <134382909+ssneharathore@users.noreply.github.com>
2025-07-23 05:08:42 +0530

    Add files via upload

---
+++ mostOccurence.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class test{
+	public static void main(String args[]){
+		int n=1222224;
+		int count=0;
+		while(n>0){
+			int rem=n%10;
+			if(rem==2){
+				count++;
+			}
+			n=n/10;
+		}
+		System.out.println(count);
+	}
+}
