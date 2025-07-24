65090a5536638351f6fe1e2cbc68049ec537465c
ssneharathore <134382909+ssneharathore@users.noreply.github.com>
2025-07-25 00:08:30 +0530

    Update reverseNO.java

--- reverseNO.java
+++ reverseNO.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
-public class test{
+public class reverseNO{
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the number to be reversed");
 		int n=sc.nextInt();
-		int ans=0;
+		int ans=0;int counter=0;
 		while(n!=0){
 			int rem=n%10;
+			counter++;
 			n=n/10;
 			ans=ans*10+rem;
 		}
 		System.out.println(ans);
+		System.out.println("no.of digits entered="+counter);
 }
 	
 }
